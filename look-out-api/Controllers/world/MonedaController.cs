8d51b2e973cb87438c821f15b644a7e36f2eea0c
agazmuri <abel.gazmuri.m@gmail.com>
2023-09-20 17:41:56 -0300

    Implementación Moneda

---
+++ look-out-api/Controllers/world/MonedaController.cs
@@ -0,0 +1,25 @@
+﻿using look.Application.interfaces.world;
+using look.Application.services.admin;
+using look.domain.entities.world;
+using Microsoft.AspNetCore.Mvc;
+
+namespace look_out_api.Controllers.world
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonedaController : BaseController<Moneda>
+    {
+        //se instancia para acceder algun servicio no generico y se asigna en el constructor
+        private readonly IMonedaService _monedaService;
+        public MonedaController(IMonedaService monedaService) : base(monedaService)
+        {
+            _monedaService = monedaService;
+        }
+
+        protected override int GetEntityId(Moneda entity)
+        {
+            // Implementa la lógica para obtener el ID de la entidad Moneda
+            return entity.MonId;
+        }
+    }
+}
---
+++ look.domain/entities/world/Moneda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace look.domain.entities.world
+{
+    public class Moneda
+    {
+        public int MonId { get; set; }
+
+        public int? MonNombre { get; set; }
+        public int? MonVigente { get; set; }
+        public int? PaiId { get; set; }
+        public Pais? Pais { get; set; }
+
+    }
+}
