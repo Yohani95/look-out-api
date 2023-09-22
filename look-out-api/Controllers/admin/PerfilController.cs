d160c6c2303db219fb011da8183b125f80ae1990
agazmuri <abel.gazmuri.m@gmail.com>
2023-09-22 16:24:42 -0300

    implementación feature_perfil

---
+++ look-out-api/Controllers/admin/PerfilController.cs
@@ -0,0 +1,26 @@
+﻿using look.Application.interfaces.admin;
+using look.Application.services.admin;
+using look.domain.entities.admin;
+using look.domain.entities.proyecto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace look_out_api.Controllers.admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilController : BaseController<Perfil>
+    {
+        //se instancia para acceder algun servicio no generico y se asigna en el constructor
+        private readonly IPerfilService _perfilService;
+        public PerfilController(IPerfilService perfilService) : base(perfilService)
+        {
+            _perfilService = perfilService;
+        }
+
+        protected override int GetEntityId(Perfil entity)
+        {
+            // Implementa la lógica para obtener el ID de la entidad Perfil
+            return entity.Id;
+        }
+    }
+}
---
+++ look.Application/services/admin/PerfilService.cs
@@ -0,0 +1,18 @@
+﻿using look.Application.interfaces.admin;
+using look.domain.entities.admin;
+using look.domain.interfaces;
+using look.domain.interfaces.admin;
+
+namespace look.Application.services.admin
+{
+    public class PerfilService : Service<Perfil>, IPerfilService
+    {
+        //instanciar repository si se requiere 
+        private readonly IPerfilRepository _perfilRepository;
+
+        public PerfilService(IPerfilRepository perfilRepository) : base(perfilRepository)
+        {
+            _perfilRepository = perfilRepository;
+        }
+    }
+}
