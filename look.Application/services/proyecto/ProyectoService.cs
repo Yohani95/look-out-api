76bd92a0b741d94b31e472244fd7b8e10661a501
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2023-09-26 09:53:53 -0300

    Revert "avances"
    This reverts commit c06150c43ccd1713b50a4c968f4663396cc8f8bb.

--- look.Application/services/proyecto/ProyectoService.cs
+++ look.Application/services/proyecto/ProyectoService.cs
@@ -1,13 +1,9 @@
 ﻿using look.Application.interfaces.proyecto;
-using look.Application.services.Common;
 using look.domain.dto.admin;
 using look.domain.entities.Common;
 using look.domain.entities.proyecto;
 using look.domain.interfaces;
 using look.domain.interfaces.proyecto;
-using look.domain.interfaces.unitOfWork;
-using Microsoft.AspNetCore.Http;
-using Serilog;
 
 namespace look.Application.services.proyecto
 {
@@ -15,56 +11,10 @@ namespace look.Application.services.proyecto
     {
         //instanciar repository si se requiere 
         private readonly IProyectoRepository _proyectoRepository;
-        private readonly ILogger _logger = Logger.GetLogger();
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IProspectoService _prospectoService;
-        public ProyectoService(IProyectoRepository proyectoRepository, IProspectoService prospectoService) : base(proyectoRepository)
+
+        public ProyectoService(IProyectoRepository proyectoRepository) : base(proyectoRepository)
         {
             _proyectoRepository = proyectoRepository;
-            _prospectoService = prospectoService;
-        }
-
-        public async Task<ServiceResult> createAsync( IFormFile file1, IFormFile file2,Proyecto proyecto)
-        {
-            try
-            {
-                _logger.Information("Crear proyecto");
-                if(proyecto == null)
-                {
-                    return new ServiceResult
-                    {
-                        IsSuccess = false,
-                        Message="",
-                        MessageCode=ServiceResultMessage.InvalidInput
-                    };
-                }
-                
-                var urlArchivo=Files.UploadFileAsync(file1, (int) proyecto.PryIdCliente);
-                var urlArchivo2 = Files.UploadFileAsync(file2, (int)proyecto.PryIdCliente);
-                if(urlArchivo.Equals("")|| urlArchivo2.Equals(""))
-                    return new ServiceResult { IsSuccess = false, Message="",MessageCode=ServiceResultMessage.InvalidInput };
-
-                _unitOfWork.BeginTransactionAsync();
-                var prospecto = new Prospecto {
-                
-                };
-                _prospectoService.AddAsync(prospecto);
-
-                _proyectoRepository.AddAsync(proyecto);
-
-                _unitOfWork.CommitAsync();
-                return new ServiceResult { MessageCode = ServiceResultMessage.Success,IsSuccess=true,Message=Message.PeticionOk };
-
-            }
-            catch (Exception ex) {
-                _unitOfWork.RollbackAsync();
-                return new ServiceResult
-                {
-                    IsSuccess = false,
-                    Message = "",
-                    MessageCode = ServiceResultMessage.InvalidInput
-                };
-            }
         }
 
         public async Task<ResponseGeneric<int>> GetLastId()
