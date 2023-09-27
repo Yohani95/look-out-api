4bf1c47c6c6740556f9b3e9417988423a792845c
Giovanni Benvenuto <giovanni.benvenuto@clave.cl>
2023-09-27 10:43:30 -0300

    add data fix error

--- look.Infrastructure/data/LookDbContext.cs
+++ look.Infrastructure/data/LookDbContext.cs
@@ -48,7 +48,6 @@ namespace look.Infrastructure.data
         public DbSet<Proyecto> Proyecto { get; set; }
         public DbSet<Propuesta> Propuesta { get; set; }
         public DbSet<ProyectoParticipante> ProyectoParticipante { get; set; }
-        public DbSet<Proyecto> Proyecto { get; set; }
         
         public DbSet<ProyectoDocumento> ProyectoDocumento { get; set; }
         
@@ -879,7 +878,7 @@ namespace look.Infrastructure.data
                     .HasForeignKey(d => d.TseId)
                     .HasConstraintName("FK_Propuesta_Tipo_Servicio");
             });
-            
+
             modelBuilder.Entity<ProyectoParticipante>(entity =>
             {
                 entity.HasKey(e => e.PpaId).HasName("PRIMARY");
@@ -909,72 +908,6 @@ namespace look.Infrastructure.data
                 entity.HasOne(d => d.Pro).WithMany()
                     .HasForeignKey(d => d.PryId)
                     .HasConstraintName("FK_Propuesta_Prospecto");
-            });
-            
-            modelBuilder.Entity<Proyecto>(entity =>
-            {
-                entity.HasKey(e => e.PryId).HasName("PRIMARY");
-                entity.ToTable("proyecto");
-                entity.HasIndex(e => e.PryIdCliente, "FK_Proyecto_Cliente");
-                entity.HasIndex(e => e.EpyId, "FK_Proyecto_Estado_Proyecto");
-                entity.HasIndex(e => e.MonId, "FK_Proyecto_Moneda");
-                entity.HasIndex(e => e.PrpId, "FK_Proyecto_Propuesta");
-                entity.HasIndex(e => e.TseId, "FK_Proyecto_Tipo_Servicio");
-                entity.Property(e => e.PryId)
-                    .ValueGeneratedNever()
-                    .HasColumnType("int(11)")
-                    .HasColumnName("pry_id");
-                entity.Property(e => e.PryNombre)
-                    .HasColumnType("varchar(50)")
-                    .HasColumnName("pry_nombre");
-                entity.Property(e => e.PrpId)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("prp_id");
-                entity.Property(e => e.EpyId)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("epy_id");
-                entity.Property(e => e.TseId)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("tse_id");
-                entity.Property(e => e.PryFechaInicioEstimada)
-                    .HasColumnType("date")
-                    .HasColumnName("pry_fecha_inicio_estimada");
-                entity.Property(e => e.PryValor)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("pry_valor");
-                entity.Property(e => e.MonId)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("mon_id");
-                entity.Property(e => e.PryIdCliente)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("pry_id_cliente");
-                entity.Property(e => e.PryFechaCierreEstimada)
-                    .HasColumnType("date")
-                    .HasColumnName("pry_fecha_cierre_estimada");
-                entity.Property(e => e.PryFechaCierre)
-                    .HasColumnType("date")
-                    .HasColumnName("pry_fecha_cierre");
-                entity.Property(e => e.PryIdContacto)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("pry_id_contacto");
-                entity.Property(e => e.PryIdContactoClave)
-                    .HasColumnType("int(11)")
-                    .HasColumnName("pry_id_contacto_clave");
-                entity.HasOne(d => d.Cliente).WithMany()
-                    .HasForeignKey(d => d.PryIdCliente)
-                    .HasConstraintName("FK_Proyecto_Cliente");
-                entity.HasOne(d => d.EstadoProyecto).WithMany()
-                    .HasForeignKey(d => d.EpyId)
-                    .HasConstraintName("FK_Proyecto_Estado_Proyecto");
-                entity.HasOne(d => d.Moneda).WithMany()
-                    .HasForeignKey(d => d.MonId)
-                    .HasConstraintName("FK_Proyecto_Moneda");
-                entity.HasOne(d => d.Propuesta).WithMany()
-                    .HasForeignKey(d => d.PrpId)
-                    .HasConstraintName("FK_Proyecto_Propuesta");
-                entity.HasOne(d => d.TpoServicio).WithMany()
-                    .HasForeignKey(d => d.TseId)
-                    .HasConstraintName("FK_Proyecto_Tipo_Servicio");
             });
             
             modelBuilder.Entity<Car>(entity =>
