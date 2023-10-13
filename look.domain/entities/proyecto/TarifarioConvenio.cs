dd16d8d47e62cc376e72329ad0a927510fbb98d9
Giovanni Benvenuto <giovanni.benvenuto@clave.cl>
2023-10-13 10:01:00 -0300

    add fix error

--- look.domain/entities/proyecto/TarifarioConvenio.cs
+++ look.domain/entities/proyecto/TarifarioConvenio.cs
@@ -2,14 +2,14 @@ namespace look.domain.entities.proyecto;
 
 public class TarifarioConvenio
 {
-    public int TcId { get; set; }
-    public int TcPerfilAsignado { get; set; }
-    public int TcTarifa { get; set; }
-    public int TcMoneda { get; set; }
-    public int TcBase { get; set; }
-    public int TcStatus { get; set; }
+    public int? TcId { get; set; }
+    public int? TcPerfilAsignado { get; set; }
+    public int? TcTarifa { get; set; }
+    public int? TcMoneda { get; set; }
+    public int? TcBase { get; set; }
+    public int? TcStatus { get; set; }
     public DateTime? TcInicioVigencia { get; set; }
     public DateTime? TcTerminoVigencia { get; set; }
-    public string ComentariosGrales { get; set; }
-    public int PRpId { get; set; }
+    public string? ComentariosGrales { get; set; }
+    public int? PRpId { get; set; }
 }
