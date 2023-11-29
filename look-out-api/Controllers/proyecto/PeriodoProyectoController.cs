22116b12abd5c8ebe6f21ff8bbcabd8d8d5112c2
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2023-11-29 10:21:31 -0300

    cambias controller

--- look-out-api/Controllers/proyecto/PeriodoProyectoController.cs
+++ look-out-api/Controllers/proyecto/PeriodoProyectoController.cs
@@ -6,12 +6,12 @@ namespace look_out_api.Controllers.proyecto
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PeriodoProyectosController: BaseController<PeriodoProyecto>
+    public class PeriodoProyectoController: BaseController<PeriodoProyecto>
     {
         //se instancia para acceder algun servicio no generico y se asigna en el constructor
         private readonly IPeriodoProyectoService _periodoProyectoService;
         
-        public PeriodoProyectosController(IPeriodoProyectoService periodoProyectoService) : base(periodoProyectoService)
+        public PeriodoProyectoController(IPeriodoProyectoService periodoProyectoService) : base(periodoProyectoService)
         {
             _periodoProyectoService = periodoProyectoService;
         }
