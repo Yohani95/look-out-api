d8ceaa8dd4c223c8a66f5ceac8d17526a8b9d7f7
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2024-02-22 11:31:22 -0300

    agregar correccion en calculo de dias y dias a pago

---
+++ look-out-api/Controllers/factura/DiaPagosController.cs
@@ -0,0 +1,21 @@
+﻿using look.Application.interfaces;
+using look.Application.interfaces.factura;
+using look.domain.entities.factura;
+using Microsoft.AspNetCore.Mvc;
+
+namespace look_out_api.Controllers.factura
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiaPagosController : BaseController<DiaPagos>
+    {
+        public DiaPagosController(IDiasPagosService service) : base(service)
+        {
+        }
+
+        protected override int GetEntityId(DiaPagos entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
