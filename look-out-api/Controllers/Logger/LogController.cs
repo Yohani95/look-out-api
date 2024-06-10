e8701753a7375814e65d19c78f8ea61c2f422ef0
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2024-06-10 14:31:13 -0400

    agregar log controlador

---
+++ look-out-api/Controllers/Logger/LogController.cs
@@ -0,0 +1,21 @@
+﻿
+using look.Application.interfaces.log;
+using look.domain.entities.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace look_out_api.Controllers.Logger
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : BaseController<LogEntry>
+    {
+        public LogController(ILogService service) : base(service)
+        {
+        }
+
+        protected override int GetEntityId(LogEntry entity)
+        {
+            return entity.Id;
+        }
+    }
+}
