ebb7444c8d0f983b83cb54555b0f52498bd08dc2
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2024-06-21 17:32:49 -0400

    agregar nueva logica para oportunidad y tablas correspondientes

---
+++ look-out-api/Controllers/oportunidad/TipoCerradaOportunidadController.cs
@@ -0,0 +1,21 @@
+﻿using look.Application.interfaces;
+using look.Application.interfaces.oportunidad;
+using look.domain.entities.oportunidad;
+using Microsoft.AspNetCore.Mvc;
+
+namespace look_out_api.Controllers.oportunidad
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoCerradaOportunidadController : BaseController<TipoCerradaOportunidad>
+    {
+        public TipoCerradaOportunidadController(ITipoCerradaOportunidadService service) : base(service)
+        {
+        }
+
+        protected override int GetEntityId(TipoCerradaOportunidad entity)
+        {
+            return entity.Id;
+        }
+    }
+}
---
+++ look.Infrastructure/repository/oportunidad/TipoLicenciaOportunidadRepository.cs
@@ -0,0 +1,19 @@
+﻿using look.domain.entities.oportunidad;
+using look.domain.interfaces.oportunidad;
+using look.Infrastructure.data;
+using MyApp.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace look.Infrastructure.repository.oportunidad
+{
+    public class TipoLicenciaOportunidadRepository : Repository<TipoLicenciaOportunidad>, ITipoLicenciaOportunidadRepository
+    {
+        public TipoLicenciaOportunidadRepository(LookDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
